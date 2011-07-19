8e09ae090e1574aa10ffc03772c33f08d6ecabd5
Richard Whitehouse <github@richardwhiuk.com>
2011-07-19 11:52:54 +0100

    Inital build with build system

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "hydra.hpp"
+
+int main(int argc, char** argv){
+
+	std::cout << "Hydra HTTP (" << HYDRA << ") Daemon" << std::endl;
+	std::cout << std::endl;
+
+	return 0;
+
+}
+
